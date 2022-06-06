66a2609b7521220fa124256071c58aa790763ef9
amirfazli.1384 <amirfazli.1384@yahoo.com>
2022-06-06 13:04:36 +0430

    django_project

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import BlogComent, BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView, BlogDetailViewComment, LikeView
+
+urlpatterns = [
+    path('post/<int:pk>/comments/', BlogDetailViewComment.as_view(), name='Comment_detail'),
+    path('post/<int:pk>/comment/', BlogComent.as_view(), name='Post_Comment'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('', BlogListView.as_view(), name='home'),
+    path('like/<int:pk>', LikeView,name="like_post"),
+]
---
+++ blog/views.py
@@ -0,0 +1,54 @@
+from dataclasses import field
+from multiprocessing import context
+from django.shortcuts import render,get_object_or_404
+from .models import Post, Comment
+
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
+
+from django.urls import reverse_lazy,reverse
+
+# Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    #def get_context_data(self,*args ,**kwargs):
+        #stuff= get_object_or_404(Post, id=self.kwargs['pk'])
+        #total_likes=stuff.total_likes()
+        #context["total_likes"] = total_likes
+        #return context
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
+class BlogComent(CreateView):
+    model = Comment
+    template_name= 'Post_Comment.html'
+    fields = ['post','comment','author']
+class BlogDetailViewComment(DetailView):
+    model = Comment
+    template_name = 'Comment_detail.html'
+
+def LikeView(request,pk):
+    post =get_object_or_404(Post, id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('post_detail', args=[int(pk)]))
+
+
